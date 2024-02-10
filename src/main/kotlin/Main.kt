d913628162e31e5fd97473cc2e56336f47ed80b2
gloria <nyagagakiigloria@gmail.com>
2024-02-10 17:50:56 +0300

    origin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var name: String= "Gloria Nyaga"
+    println (name)
+
+    var age: Byte = (20)
+    println (age)
+
+    var weight: Float = 63.48F
+    println (weight)
+
+    var phoneNumber: String = "(+254)700345656"
+    println (phoneNumber)
+
+    var citizen: String= "Kenyan"
+    println (citizen)
+
+    var Kenyan: Boolean = true
+    println (Kenyan)
+
+
+
+
+}
